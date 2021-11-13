44452ac4c3f16144565a2d7c34956c9dd9f8286a
StanHus <guseletovS@gmail.com>
2021-11-13 16:39:18 +0000

    1.2.1 Header updated

--- src/components/Landing_Page.tsx
+++ src/components/Landing_Page.tsx
@@ -5,7 +5,7 @@ export default function Landing() {
   return (
     <Fragment>
       <div className="landing-body">
-        <h1 className="title">Battle Royale</h1>
+        <h1 className="title">Elimination Tournament Generator</h1>
         <p className="subTitle">
           Ok, so I love the process of elimination. I also love empirically
           finding out what which song/episode/character I really think is best.
