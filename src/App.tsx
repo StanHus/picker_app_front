be73ab6913da2f3eeb14a6b447dcdee1750139da
StanHus <guseletovS@gmail.com>
2021-11-16 16:40:40 +0000

    stars in the works

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Game from "./components/Game/Game";
 import InputTitles from "./components/InputPage";
@@ -6,25 +7,27 @@ import "./css/style.css";
 
 export default function App() {
   return (
-    <div className="background">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-      <BrowserRouter>
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Landing />
-            </Route>
-            <Route exact path="/input">
-              <InputTitles />
-            </Route>
-            <Route exact path="/game">
-              <Game />
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
+    <section>
+      <div className="background">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <BrowserRouter>
+          <main>
+            <Switch>
+              <Route exact path="/">
+                <Landing />
+              </Route>
+              <Route exact path="/input">
+                <InputTitles />
+              </Route>
+              <Route exact path="/game">
+                <Game />
+              </Route>
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </div>
+    </section>
   );
 }
